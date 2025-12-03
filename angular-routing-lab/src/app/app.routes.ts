f01fd024f7796d92e5f4319ec4a7bc17c8994e10
Princess Angela Lacanlale <lacanlaleprincess28@gmail.com>
2025-12-03 16:45:27 +0800

    Add files via upload

--- angular-routing-lab/src/app/app.routes.ts
+++ angular-routing-lab/src/app/app.routes.ts
@@ -2,11 +2,20 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Directives } from './directives/directives';
 import { DataBinding } from './data-binding/data-binding';
+import { Bootstrap } from './bootstrap/bootstrap';
+import { Tailwind } from './tailwind/tailwind';
+import { Pagenotfound } from './pagenotfound/pagenotfound';
+
+
 
 export const routes: Routes = [
   { path: '', component: Home},
   { path: 'directives', component: Directives},
-  { path: 'data-binding', component: DataBinding}
+  { path: 'data-binding', component: DataBinding},
+  { path:'bootstrap', component: Bootstrap},
+  { path:'tailwind', component: Tailwind},
+  { path: '**', component: Pagenotfound}
+
 ];
 
 
